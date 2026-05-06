bbd16e8de0675d3bdd38c2ecf2dee9fbffaf399f
Nithya Vupparapalli <nithya.vupparapalli@blupantera.com>
2026-05-06 13:53:41 +1000

    Added vercel link

--- profiler/backend/main.py
+++ profiler/backend/main.py
@@ -10,14 +10,14 @@ app = FastAPI(title="Data Profiler API")
 
 app.add_middleware(
     CORSMiddleware,
-    # allow_origins=[
-    #     "http://localhost:5173",
-    #     "http://127.0.0.1:5173",
-    # ],
+     allow_origins=[
+         "http://localhost:5173",
+         "http://127.0.0.1:5173",
+         "https://mvp-nine-woad.vercel.app/",
+     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-    allow_origins=["*"]
 )
 
 TYPE_DETECTION_SAMPLE_SIZE = 1000
